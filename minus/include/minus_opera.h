f043b369268f096a4d23d5eb133649ec214edeff
leo <leoking02@foxmail.com>
2022-10-11 11:56:52 +0800

    modi ok. run ok.

---
+++ minus/include/minus_opera.h
@@ -0,0 +1,7 @@
+#ifndef MINUS__H
+#define MINUS__H
+
+int minus(int, int);
+double minus(double, double);
+int minus_my(int, int);
+#endif
---
+++ minus/minus_opera.cc
@@ -0,0 +1,14 @@
+#include "minus_opera.h"
+
+int minus(int a, int b){
+    return a-b;
+}
+
+int minus_my(int a, int b){
+    return a-b;
+}
+
+
+double minus(double a, double b){
+    return a-b;
+}
